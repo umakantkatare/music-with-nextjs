cfb917151835d79e270f3ee1220cbf52c755a834
umakant katare <umakantkatare11@gmail.com>
2026-07-18 10:31:00 +0530

    Add Instructor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import TestimonialCards from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import { WhyChooseUs } from "@/components/WhyChooseUs";
@@ -11,7 +12,8 @@ export default function Home() {
       <FeaturedCourses />
       <WhyChooseUs />
       <TestimonialCards />
-      <UpcomingWebinars/>
+      <UpcomingWebinars />
+      <Instructors />
     </main>
   );
 }
